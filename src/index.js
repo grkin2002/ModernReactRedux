224a9da0277a70ac4291a62aded871bcf3178c29
greg pong <grkin2002@gmail.com>
2018-08-03 09:32:29 +1000

    More on JSX

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//Create a new component, This component should produce some HTML
+const App = () => {
+  return <div>Hi There  !</div>
+}
+
+//Take this component's generated HTML and put it on the page (in the DOM)
